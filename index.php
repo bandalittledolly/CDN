ad8131286c0549bf344a8f89deee1566e4883fbb
geovanedev5 <148703357+geovanedev5@users.noreply.github.com>
2023-10-31 17:33:16 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hospedagem de Arquivos</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body {
+            background-color: #222;
+            color: #fff;
+            font-family: Arial, sans-serif;
+        }
+
+        .container {
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+            text-align: center;
+        }
+
+        .btn-custom {
+            background-color: #3498db;
+            color: #fff;
+        }
+
+        .btn-custom:hover {
+            background-color: #2980b9;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Esta é uma hospedagem de arquivos do site: https://littledolly.com.br</h1>
+        <a href="https://littledolly.com.br" class="btn btn-custom btn-lg">Ir para o Site</a>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
